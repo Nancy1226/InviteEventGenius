c5f17a5459eabdd56de4c24045f0a06faeb85a73
nancy-12_03@hotmail.com <nancy-12_03@hotmail.com>
2023-08-16 12:06:17 -0600

    ADD Template

---
+++ src/pages/EditTemplate.jsx
@@ -0,0 +1,12 @@
+import BodyEditTemplate from "../components/organism/BodyEditTemplate";
+
+function EditTemplate() {
+    return ( 
+    <>
+        <h1>Hola soy la page</h1>
+        <BodyEditTemplate />
+    </>
+     );
+}
+
+export default EditTemplate;
